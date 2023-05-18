0f1569b8235c207277f227f1611f3bba1015e289
sahahn <sahforever82@gmail.com>
2023-05-18 13:20:13 -0400

    Add dist

---
+++ dist/index.js
@@ -0,0 +1,111 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 494:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 627:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(494);
+const github = __nccwpck_require__(627);
+
+function listPullRequests(token, repoOwner, repo, state) {
+  const octokit = github.getOctokit(token);
+  var pullRequests = octokit.rest.pulls.list({
+    owner: repoOwner,
+    repo: repo,
+    state: state,
+    sort: 'updated',
+    direction: 'desc',
+    per_page: 100,
+  });
+  return pullRequests;
+}
+
+function filterDate(pr, targetDate) {
+  var createdAt =new Date(pr.created_at)
+  if (createdAt > targetDate) {
+    return true;
+  }
+  return false;
+}
+
+function outputNumbers(list) {
+  let numberList = list.map(p => p.number);
+  core.setOutput('pullRequestNumbers', numberList);
+}
+
+
+try {
+  const token = core.getInput('token');
+  const repoOwner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  const state = core.getInput('state');
+  let filterMs = parseInt(core.getInput('window')) * 3600000 // convert to milliseconds
+  let targetDate = new Date(Date.now() - filterMs);
+  
+  let prom = listPullRequests(token, repoOwner, repo, state);
+  prom.then(function (list) {
+    let filtered = list.data.filter(function(pr) { return filterDate(pr, targetDate); });
+    outputNumbers(filtered);
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
